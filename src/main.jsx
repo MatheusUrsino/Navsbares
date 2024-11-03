000bae24d2a5c81ffde1813257b7aac04993b630
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-11-03 15:32:52 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ClerkProvider } from '@clerk/clerk-react'
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = "pk_live_Y2xlcmsubWF0aGV1c3Vyc2luby5naXRodWIuaW8k"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk publishable key to the .env.local file')
